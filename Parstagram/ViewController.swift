c08372fd57ff6f5aec2f06fd0d38f27e86764e18
Prerana Sunilkumar <preranasunilkumar@preranas-macbook-pro.local>
2019-03-09 10:21:16 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Prerana Sunilkumar on 3/9/19.
+//  Copyright © 2019 Prerana Sunilkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
